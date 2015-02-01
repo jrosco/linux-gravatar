0c62ad68fd829f522cdce18cb8f9baaf59a79137
Joel Cumberland <joel_c@zoho.com>
2015-02-02 10:34:40 +1100

    -

--- src/trayicon.py
+++ src/trayicon.py
@@ -9,6 +9,8 @@ import appindicator
 import gravatar
 import settings
 import _version as version
+import os
+import sys
 
 
 class StartTrayIcon():
@@ -19,6 +21,7 @@ class StartTrayIcon():
         self.gravatar = gravatar.Gravatar()
         self.icon = '/usr/share/icons/gravatar.png'
         self.setting_menu_icon = '/usr/share/linux-gravatar/settings_menu_icon.png'
+        self.refresh_menu_icon = '/usr/share/linux-gravatar/refresh_menu_icon.png'
         self.about_menu_icon = '/usr/share/linux-gravatar/about_menu_icon.png'
         self.close_menu_icon = '/usr/share/linux-gravatar/close_menu_icon.png'
         self.profile_img = '/home/jrosco/.face'
@@ -48,6 +51,16 @@ class StartTrayIcon():
         SettingsDialog().run_dialog()
 
     @staticmethod
+    def refresh(item):
+
+        print 'called refresh() %s' % item
+        """Restarts the current program.
+        Note: this function does not return. Any cleanup action (like
+        saving data) must be done before calling this function."""
+        python = sys.executable
+        os.execl(python, python, * sys.argv)
+
+    @staticmethod
     def about(item):
 
         print 'called about() %s' % item
@@ -73,23 +86,31 @@ class StartTrayIcon():
         menu = gtk.Menu()
 
         """ Testing profile pic in menu """
-        # profile_icon = gtk.Image()
-        # profile_icon.set_from_file(self.profile_img)
-        # menu_items = gtk.ImageMenuItem('joel.cumberland@insp.com.au')
-        # #menu_items.connect('activate', self.menu_settings)
-        # menu_items.set_image(profile_icon)
-        # menu.append(menu_items)
-        # menu_items.show()
-        #
-        # sep = gtk.SeparatorMenuItem()
-        # menu.append(sep)
-        # sep.show()
+        profile_icon = gtk.Image()
+        profile_icon.set_from_file(self.profile_img)
+        menu_items = gtk.ImageMenuItem('CURRENT PROFILE')
+        #menu_items.connect('activate', self.menu_settings)
+        menu_items.set_image(profile_icon)
+        menu.append(menu_items)
+        menu_items.show()
+
+        sep = gtk.SeparatorMenuItem()
+        menu.append(sep)
+        sep.show()
 
         setting_icon = gtk.Image()
         setting_icon.set_from_file(self.setting_menu_icon)
         menu_items = gtk.ImageMenuItem('Settings')
         menu_items.connect('activate', self.menu_settings)
         menu_items.set_image(setting_icon)
+        menu.append(menu_items)
+        menu_items.show()
+
+        refresh_icon = gtk.Image()
+        refresh_icon.set_from_file(self.refresh_menu_icon)
+        menu_items = gtk.ImageMenuItem('Refresh')
+        menu_items.connect('activate', self.refresh)
+        menu_items.set_image(refresh_icon)
         menu.append(menu_items)
         menu_items.show()
 
@@ -149,8 +170,8 @@ class SettingsDialog(StartTrayIcon):
         self.config_values.save_to_config('email', self.email_txt.get_text())
         self.config_values.save_to_config('check', self.check_int.get_value())
         self.settings_dialog.destroy()
-        #print self.gravatar
-        self.refresh_gravatar()
+        #self.refresh_gravatar()
+        self.refresh(None)
 
     def close_settings_win(self, widget):
 
